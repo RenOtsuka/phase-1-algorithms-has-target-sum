348a8da34bd33e4b8b6108f5c8c7392630d66afa
Ryan Delos Santos <ryansantos.delossantos121@gmail.com>
2022-01-25 11:39:19 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for(let i = 0; i < array.length; i++){
+    for(let j = i + 1; j < array.length; j++){
+      if(array[i] + array[j] === target){
+        return true;
+      }
+      else{
+        continue;
+      }
+    }
+  }
+
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+  O(n^2)
 */
 
 /* 
   Add your pseudocode here
+  Initialize the outer loop, which will keep track of the number we're comparing to
+    Initialize the inner loop to iterate through the rest of the array
+      Checks if the sum of the first and second number equals to the target number
+      If true, then it will retuen true
+      If false, it will continue iterating
+  If the entire array has been iterated through w/o returning true,
+  then it will return false
 */
 
 /*
   Add written explanation of your solution here
+  I use a nested loop to keep track of the first number we're comparing to, and compare it
+  to the rest of the array by iterating with the inner loop
+  'i' is the index of the number we're comparing to the rest of the array
 */
 
 // You can run `node index.js` to view these console logs
